8986cff825267fa883800bef2b095b3a28d175b9
jiyuujin <jiyuujinunite@gmail.com>
2020-11-15 08:11:15 +0900

    use grave accent

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,83 +3,83 @@ module.exports = {
   description: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。`,
   head: [
     [
-      'meta',
-      { hid: 'description', name: 'description', content: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。` },
+      `meta`,
+      { hid: `description`, name: `description`, content: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。` },
     ],
     [
-      'meta',
-      { property: 'og:site_name', content: `Flutter Osaka | ドキュメント一覧` },
+      `meta`,
+      { property: `og:site_name`, content: `Flutter Osaka | ドキュメント一覧` },
     ],
     [
-      'meta',
-      { property: 'og:url', content: 'https://flutter-osaka.netlify.app/' },
+      `meta`,
+      { property: `og:url`, content: `https://flutter-osaka.netlify.app/` },
     ],
     [
-      'meta',
-      { property: 'og:type', content: 'website' },
+      `meta`,
+      { property: `og:type`, content: `website` },
     ],
     [
-      'meta',
-      { property: 'og:title', content: `Flutter Osaka | ドキュメント一覧` },
+      `meta`,
+      { property: `og:title`, content: `Flutter Osaka | ドキュメント一覧` },
     ],
     [
-      'meta',
-      { property: 'og:description', content: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。` },
+      `meta`,
+      { property: `og:description`, content: `ミートアップやハンズオンでご紹介のあった資料を中心に記録しています。` },
     ],
   ],
   script: [
     {
       async: true,
-      src: '//www.youtube.com/iframe_api'
+      src: `//www.youtube.com/iframe_api`
     },
   ],
-  base: '/',
-  docsDir: 'docs',
+  base: `/`,
+  docsDir: `docs`,
   serviceWorker: true,
   themeConfig: {
-    repo: 'flutter-osaka/docs',
+    repo: `flutter-osaka/docs`,
     editLinks: false,
-    docsDir: 'docs',
+    docsDir: `docs`,
     nav: [
       {
-        text: 'ミートアップ',
-        link: '/meetup/osaka',
+        text: `ミートアップ`,
+        link: `/meetup/osaka`,
       }
     ],
     sidebarDepth: 3,
     sidebar: {
       '/': [
         {
-          title: 'Flutter Osaka',
+          title: `Flutter Osaka`,
           collapsable: false,
           children: [
-            '/',
-            '/meetup/osaka',
-            '/handson/basic',
-            '/handson/dictionary',
-            '/handson/picture_edit',
-            '/handson/webrtc'
+            `/`,
+            `/meetup/osaka`,
+            `/handson/basic`,
+            `/handson/dictionary`,
+            `/handson/picture_edit`,
+            `/handson/webrtc`
           ]
         }
       ],
       '/meetup/': [
         {
-          title: 'ミートアップ',
+          title: `ミートアップ`,
           collapsable: false,
           children: [
-            '/osaka'
+            `/osaka`
           ]
         }
       ],
       '/handson/': [
         {
-          title: 'ハンズオン',
+          title: `ハンズオン`,
           collapsable: false,
           children: [
-            '/basic',
-            '/dictionary',
-            '/picture_edit',
-            '/webrtc'
+            `/basic`,
+            `/dictionary`,
+            `/picture_edit`,
+            `/webrtc`
           ]
         }
       ]
